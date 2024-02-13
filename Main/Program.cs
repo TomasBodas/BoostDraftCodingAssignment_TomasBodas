a4bf014be5c04e7d0e0db5675ec625b033931ab8
TomasBodas <tomasbodas20@gmail.com>
2024-02-14 00:48:51 +0900

    Add project files.

---
+++ Main/Program.cs
@@ -0,0 +1,23 @@
+﻿using SimpleXMLValidatorLibrary;
+
+class Program
+{
+    //you can use here to test, feel free to modify/add the test cases here.
+    //you can also use other ways to test if you want
+    static void Main(string[] args)
+    {
+        List<(string testCase, bool expectedResult)> testCases = new()
+        {
+            ("<Design><Code>hello world</Code></Design>",  true),//normal case
+            ("<Design><Code>hello world</Code></Design><People>", false),//no closing tag for "People" 
+            ("<People><Design><Code>hello world</People></Code></Design>", false),// "/Code" should come before "/People" 
+            ("<People age=”1”>hello world</People>", false),//there is no closing tag for "People age=”1”" and no opening tag for "/People"
+        };
+        for (int i = 0; i < testCases.Count; i++)
+        {
+            (string testCase, bool expectedResult) = testCases[i];
+            bool isValid = SimpleXmlValidator.DetermineXml(testCase);
+            Console.WriteLine($"Is a valid XML string? {isValid}, Expected result = {expectedResult}");
+        }
+    }
+}
---
+++ SimpleXMLValidatorLibrary/SimpleXmlValidator.cs
@@ -0,0 +1,56 @@
+﻿namespace SimpleXMLValidatorLibrary
+{
+    public class SimpleXmlValidator
+    {
+        public static bool DetermineXml(string xml)
+        {
+            Stack<string> tagStack = new Stack<string>();
+
+            int index = 0;
+            while (index < xml.Length)
+            {
+                //Find the next opening tag
+                int startTagStartIndex = xml.IndexOf('<', index);
+                if (startTagStartIndex == -1)
+                {
+                    break; //No more opening tags found, exit loop
+                }
+
+                int startTagEndIndex = xml.IndexOf('>', startTagStartIndex);
+                if (startTagEndIndex == -1)
+                {
+                    return false; //Opening tag not properly formed
+                }
+
+                string startTag = xml.Substring(startTagStartIndex + 1, startTagEndIndex - startTagStartIndex - 1);
+                index = startTagEndIndex + 1;
+
+                //Check if it's a closing tag
+                if (startTag.StartsWith("/"))
+                {
+                    if (tagStack.Count == 0 || startTag.Substring(1) != tagStack.Pop())
+                    {
+                        return false; //Closing tag does not match last opening tag
+                    }
+                }
+                else
+                {
+                    //It's an opening tag, push it onto the stack
+                    tagStack.Push(startTag);
+
+                    //Find the corresponding closing tag
+                    string endTag = $"</{startTag}>";
+                    int endTagIndex = xml.IndexOf(endTag, index);
+                    if (endTagIndex == -1)
+                    {
+                        return false; //No corresponding closing tag found
+                    }
+                    index = endTagIndex + endTag.Length;
+                }
+            }
+
+            //If all checks out return true
+            return true;
+        }
+    }
+}
